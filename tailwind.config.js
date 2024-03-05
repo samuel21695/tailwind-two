91370ce7179c52b17fa59e7f148ee2f299854b84
samuel21695 <samuel21695@gmail.com>
2024-03-05 12:47:35 +0900

    🧪 tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,11 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  purge: [
+    // 프로젝트에 사용되는 HTML 파일의 경로를 여기에 추가하세요.
+    './index.html',
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
 }
 
